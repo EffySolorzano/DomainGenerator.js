1c7d36524ba4cf0a9f5f09d069aad96e3b3244fc
EffySolorzano <solorzano.steph@gmail.com>
2023-02-16 20:30:18 +0000

    finalupdate!!!

--- src/app.js
+++ src/app.js
@@ -6,6 +6,29 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#btn").addEventListener("click", execute);
+};
+
+let pronoun = ["my", "the", "our", "thy"];
+let adj = ["great", "big", "chunky"];
+let noun = ["jogger", "racoon", "puppy", "chicken"];
+let dom = [".com", ".us", ".org", ".net"];
+
+const execute = () => {
+  let results = document.getElementById("resultados-dominios");
+  results.innerHTML = ""; // clear the previous results
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let l = 0; l < dom.length; l++) {
+          let domain = pronoun[i] + adj[j] + noun[k] + dom[l];
+          let prgh = document.createElement("p");
+          let text = document.createTextNode(domain);
+          prgh.appendChild(text);
+          results.appendChild(prgh);
+        }
+      }
+    }
+  }
 };
